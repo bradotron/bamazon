cbd6921e8d1c1d62bcfdba90a5cf2d294b2ccd7e
Bradley Jensen <bradley13@gmail.com>
2019-03-11 19:43:20 -0400

    node initialized and packages added

---
+++ bamazonCustomer.js
@@ -0,0 +1,11 @@
+// load the inquirer library
+var inquirer = require(`inquirer`);
+// load the mysql library
+var mysql = require(`mysql`);
+
+
+
+
+
+// ------------ CODE EXECUTION BELOW THIS POINT ------------- //
+console.log("hello world");
